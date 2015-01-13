3dbaacab7e98a6e411dbf2db64d3dd1a0472ac3b
Jasper Westra <jasper.westra@live.nl>
2015-01-13 16:42:11 +0100

    Build 1

--- Clients/NoteFinder-PC/src/notefinder/pc/NoteFinderPC.java
+++ Clients/NoteFinder-PC/src/notefinder/pc/NoteFinderPC.java
@@ -12,6 +12,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -30,19 +33,17 @@ public class NoteFinderPC extends Application {
         
         MenuBar menuBar = new MenuBar();
         
-        Menu mf = new Menu("Import +");
-        Menu mfo = new Menu("Open image file");
-        Menu mfs = new Menu("Scan image");
+        Menu mf = new Menu("Import", new ImageView(new Image("img/camera104.png")));
+        MenuItem mfo = new MenuItem("Open image file");
+        MenuItem mfs = new MenuItem("Scan image");
         mf.getItems().addAll(mfo, mfs);
         
-        Menu me = new Menu("Export +");
-        Menu me3 = new Menu("To mp3");
-        Menu mem = new Menu("To midi");
+        Menu me = new Menu("Export", new ImageView(new Image("img/note54.png")));
+        MenuItem me3 = new MenuItem("To mp3");
+        MenuItem mem = new MenuItem("To midi");
         me.getItems().addAll(me3, mem);
         
-        
-        
-        Menu ms = new Menu("Settings");
+        Menu ms = new Menu("Settings", new ImageView(new Image("img/repair17.png")));
         
         menuBar.getMenus().addAll(mf, me, ms);
         
