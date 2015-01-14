d5066f82ff0a0eb9d7e7f1f5f5676e6c9995d1e7
Jasper Westra <jasper.westra@live.nl>
2015-01-14 16:51:37 +0100

    Added neural network

--- Clients/NoteFinder-PC/src/notefinder/pc/NoteFinderPC.java
+++ Clients/NoteFinder-PC/src/notefinder/pc/NoteFinderPC.java
@@ -1,23 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package notefinder.pc;
 
+import javafx.animation.FadeTransition;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
+import javafx.scene.web.WebView;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 /**
  *
@@ -27,9 +33,30 @@ public class NoteFinderPC extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        
+        showSplash(primaryStage);
+    }
+    
+    private Pane splashLayout;
+    private ProgressBar loadProgress;
+    private Label progressText;
+    private WebView webView;
+    private Stage mainStage;
+    private static final int SPLASH_WIDTH = 400;
+    private static final int SPLASH_HEIGHT = 400; 
+    
+    @Override public void init() {
+        ImageView splash = new ImageView(new Image("img/LogoSmall.png"));
+        loadProgress = new ProgressBar();
+        loadProgress.setPrefWidth(SPLASH_WIDTH);
+        progressText = new Label("Loading hobbits with pie . . .");
+        splashLayout = new VBox();
+        splashLayout.getChildren().addAll(splash, loadProgress, progressText);
+        progressText.setAlignment(Pos.CENTER);
+        splashLayout.setEffect(new DropShadow());
+    } 
+    
+    private void showMainStage() {
         Scene scene = new Scene(new VBox(), 800, 600);
-        scene.setFill(Color.OLDLACE);
         
         MenuBar menuBar = new MenuBar();
         
@@ -58,11 +85,54 @@ public class NoteFinderPC extends Application {
         });
         
         ((VBox) scene.getRoot()).getChildren().addAll(menuBar);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
+    
+    private void hideSplash(Stage initStage)
+    {
+         if (initStage.isShowing()) {
+            loadProgress.progressProperty().unbind();
+            loadProgress.setProgress(1);
+            progressText.setText("All hobbits are full.");
+            initStage.setIconified(false);
+            initStage.toFront();
+            FadeTransition fadeSplash = new FadeTransition(Duration.seconds(1.2), splashLayout);
+            fadeSplash.setFromValue(1.0);
+            fadeSplash.setToValue(0.0);
+            fadeSplash.setOnFinished(new EventHandler<ActionEvent>() {
+                @Override public void handle(ActionEvent actionEvent) {
+                    initStage.hide();
+                    showMainStage();
+                }
+            });
+            fadeSplash.play();
+        } 
+    }
+
+    private void showSplash(Stage initStage) {
+        Scene splashScene = new Scene(splashLayout);
+        initStage.initStyle(StageStyle.TRANSPARENT);
+        final Rectangle2D bounds = Screen.getPrimary().getBounds();
+        initStage.setScene(splashScene);
+        initStage.setX(bounds.getMinX() + bounds.getWidth() / 2 - SPLASH_WIDTH / 2);
+        initStage.setY(bounds.getMinY() + bounds.getHeight() / 2 - SPLASH_HEIGHT / 2);
+        initStage.setTitle("NoteFinder");
+        initStage.show();
+        
+        new java.util.Timer().schedule( 
+            new java.util.TimerTask() {
+                @Override
+                public void run() {
+                    Platform.runLater(new Runnable() {
+                        @Override
+                        public void run() {
+                            hideSplash(initStage);
+                        }
+                   });
+                }
+            }, 
+            5000 
+        );
+    } 
 
     /**
      * @param args the command line arguments
