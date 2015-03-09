04d924900546eaa275196404855d727305da785e
Jasper Westra <jasper.westra@live.nl>
2015-03-09 12:51:10 +0100

    Java versie visionlab
    ;)

---
+++ Java/Opencv2Test/src/opencv2test/Analyse.java
@@ -0,0 +1,280 @@
+package opencv2test;
+
+import java.util.ArrayList;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+public class Analyse
+{
+    public static Mat convertToGrey (Mat img)
+    {
+        Imgproc.cvtColor(img, img, 7);
+        return img;
+    }
+    
+    public static void thresholdISOBlack (Mat src, Mat trg)
+    {
+        Imgproc.threshold(src, trg, 150, 255, 0);
+    }
+    
+    public static void printImage(Mat bron)
+    {
+        for (int r = 0; r < bron.rows(); r++)
+        {
+            for (int c = 0; c < bron.cols(); c++)
+            {
+                System.out.print(bron.get(r, c)[0]);
+            }
+            System.out.print('\n');
+        }
+    }
+    
+    public static Mat averageRows (Mat bron)
+    {
+        Mat rowCounted = new Mat(bron.rows(), 1, 16);
+	Imgproc.cvtColor(rowCounted, rowCounted, 7); // Hacky methode om een leeg zwartwit plaatje te krijgen.
+	for (int r = 0; r < bron.rows(); r++)
+	{
+            int count = 0;
+            for (int c = 0; c < bron.cols(); c++)
+            {
+                count += bron.get(r, c)[0];
+            }
+            
+            int v = Math.round(count / bron.cols());
+            rowCounted.put(r, 0, new double []{v,v,v});
+        }
+        
+	return rowCounted;
+    }
+    
+    public static Mat averageCols (Mat bron)
+    {
+        Mat colCounted = new Mat(100, bron.cols(), 16);
+        Imgproc.cvtColor(colCounted, colCounted, 7); // Hacky methode om een leeg zwartwit plaatje te krijgen.
+
+        for (int c = 0; c < bron.cols(); c++)
+        {
+            int count = 0;
+            for (int r = 0; r < bron.rows(); r++)
+            {
+                count += bron.get(r, c)[0];
+            }
+            colCounted.get(0, c)[0] = Math.round(count / bron.rows()); // Gemmiddelde uitrekenen
+            
+            int v = Math.round(count / bron.rows());
+            for (int i = 0; i < 100; i++)
+                colCounted.put(i, c, new double []{v,v,v});
+        }
+
+        return colCounted;
+    }
+    
+    public static ArrayList<ArrayList<Integer>> oneDimensionalHorizontalBlobFinder (Mat bron)
+    {
+        ArrayList<Integer> found = new ArrayList<>();
+
+        int count = 0;
+        int mean = 0;
+        int thresh_blob;
+
+        for (int r = 0; r < bron.rows(); r++)
+        {
+            int c = (int)bron.get(r, 0)[0];
+            if (c < 127) {
+                if (found.size() > 1)
+                {
+                    mean += (r - found.get(count));
+                    count++;
+                }
+                found.add(r);
+            }
+        }
+
+        ArrayList<ArrayList<Integer>> blobList = new ArrayList<>();
+
+        thresh_blob = Math.round (mean / count);
+        int minval = found.get(0);
+        int maxval = 0;
+        int i;
+        
+        for (i = 1; i < found.size() - 1; i++)
+        {
+            if (found.get(i) - found.get(i - 1) > thresh_blob)
+            {
+                ArrayList<Integer> temp = new ArrayList<>();
+
+                maxval = found.get (i - 1);
+
+                temp.add(minval);
+                temp.add(maxval);
+
+                blobList.add(temp);
+
+                minval = found.get(i);
+            }
+        }
+        ArrayList<Integer> temp = new ArrayList<>();
+        temp.add(minval);
+        maxval = found.get(i);
+        temp.add(maxval);
+        blobList.add(temp);
+
+        return blobList;
+    }
+    
+    public static ArrayList<ArrayList<Integer>> oneDimensionalVerticalBlobFinder (Mat bron)
+    {
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+        ArrayList<Integer> found;
+        
+        int pointer = 0;
+        int min = -1;
+        
+        while (pointer < bron.cols()-1)
+        {
+            if (min < 0)
+            {
+                min = pointer;
+                double a = bron.get(0, min)[0];
+                if (a > 253.0)
+                {
+                    min = -1;
+                }
+            }
+            else
+            {
+                double a = bron.get(0, pointer)[0];
+                if (a > 253.0)
+                {
+                    found = new ArrayList<>();
+                    found.add(min);
+                    found.add(pointer);
+                    list.add(found);
+                    min = -1;
+                }
+            }
+            pointer++;
+        }
+        
+        double a = bron.get(0, pointer)[0];
+        if (a > 127.0)
+        {
+            found = new ArrayList<>();
+            found.add(min);
+            found.add(pointer);
+            list.add(found);
+        }
+        
+        return list;
+    }
+    
+    public static void Erosion (Mat bron, int dilation_size, int dilation_type)
+    {
+
+      Mat element = Imgproc.getStructuringElement ( dilation_type,
+                                           new Size (2*dilation_size + 1, 2*dilation_size+1 ),
+                                           new Point (dilation_size, dilation_size ) );
+      /// Apply the dilation operation
+      Imgproc.erode(bron, bron, element );
+    }
+    
+    public static Mat filterDifference (Mat src, Mat filter)
+    {
+        //thresholdISOBlack(filter, filter);
+        Mat nw = new Mat(src.rows(), src.cols(), 16);
+        convertToGrey(nw);
+        convertToGrey(src);
+
+        for (int c = 0; c < src.cols(); c++)
+        {
+            for (int r = 0; r < src.rows(); r++)
+            {
+                //nw.at<uchar>(r, c) = (src.at<uchar>(r, c) < filter.at<uchar>(r, 0)) ? 254 : 0;
+                //nw.at<uchar>(r, c) = filter.at<uchar>(r, 0);
+                
+                boolean a = (filter.get(r, 0)[0] > 180) ? true : false;
+                boolean b = (src.get(r, c)[0] < 127) ? true : false;
+                
+                if (a && b)
+                {
+                    nw.put(r, c, new double[]{0,0,0});
+                }
+                else
+                {
+                    nw.put(r, c, new double[]{254,254,254});
+                }
+            }
+        }
+        
+        Erosion(nw, 1, 0);
+        
+        return nw;
+    }
+
+    public static void drawOneDimensionalBlobsHorizontal(ArrayList<ArrayList<Integer>> blobList, Mat img)
+    {
+        for (int i = 0; i < blobList.size(); i++)
+        {
+            int margin = Math.round ((blobList.get(i).get(1) - blobList.get(i).get(0)) / 4);
+            
+                     
+            Core.line(img, new Point(0, blobList.get(i).get(0) - margin * 3), new Point(img.cols(), blobList.get(i).get(0) - margin * 3), new Scalar(255, 0, 0), 3, 8, 0);
+            Core.line(img, new Point(0, blobList.get(i).get(1) + margin * 3), new Point(img.cols(), blobList.get(i).get(1) + margin * 3), new Scalar(0, 255, 0), 3, 8, 0);
+
+            Core.line(img, new Point(0, blobList.get(i).get(0) - margin * 2), new Point(img.cols(), blobList.get(i).get(0) - margin * 2), new Scalar(0, 0, 255), 1, 8, 0);
+            Core.line(img, new Point(0, blobList.get(i).get(1) + margin * 2), new Point(img.cols(), blobList.get(i).get(1) + margin * 2), new Scalar(0, 0, 255), 1, 8, 0);
+
+            Core.line(img, new Point(0, blobList.get(i).get(0) - margin), new Point(img.cols(), blobList.get(i).get(0) - margin), new Scalar(0, 0, 255), 1, 8, 0);
+            Core.line(img, new Point(0, blobList.get(i).get(1) + margin), new Point(img.cols(), blobList.get(i).get(1) + margin), new Scalar(0, 0, 255), 1, 8, 0);
+
+            Core.line(img, new Point(0, blobList.get(i).get(0)), new Point(img.cols(), blobList.get(i).get(0)), new Scalar(0, 0, 255), 1, 8, 0);
+            Core.line(img, new Point(0, blobList.get(i).get(1)), new Point(img.cols(), blobList.get(i).get(1)), new Scalar(0, 0, 255), 1, 8, 0);
+
+            Core.line(img, new Point(0, blobList.get(i).get(0) + margin), new Point(img.cols(), blobList.get(i).get(0) + margin), new Scalar(0, 0, 255), 1, 8, 0);
+            Core.line(img, new Point(0, blobList.get(i).get(1) - margin), new Point(img.cols(), blobList.get(i).get(1) - margin), new Scalar(0, 0, 255), 1, 8, 0);
+
+            Core.line(img, new Point(0, blobList.get(i).get(0) + margin * 2), new Point(img.cols(), blobList.get(i).get(0) + margin * 2), new Scalar(0, 0, 255), 1, 8, 0);    
+        }
+    }
+    
+    public static void drawOneDimensionalBlobsVertical(ArrayList<ArrayList<Integer>> blobList, Mat img)
+    {
+        for (int i = 0; i < blobList.size(); i++)
+        {
+            Core.line(img, new Point(blobList.get(i).get(0), 5), new Point(blobList.get(i).get(0), img.rows()-5), new Scalar(0, 255, 255));
+            Core.line(img, new Point(blobList.get(i).get(1), 5), new Point(blobList.get(i).get(1), img.rows()-5), new Scalar(0, 255, 255));
+        }
+    }
+
+    public static ArrayList<Mat> getROIperBlob(ArrayList<ArrayList<Integer>> blobList, Mat img)
+    {
+        ArrayList<Mat> rois = new ArrayList<>();
+
+        for (int i = 0; i < blobList.size(); i++)
+        {
+            int margin = Math.round ((blobList.get(i).get(1) - blobList.get(i).get(0)) / 4);
+
+            int x = 0;
+            int y = blobList.get(i).get(0) - margin * 3;
+            int width = img.cols();
+            int height = (blobList.get(i).get(1) + margin * 3) - (blobList.get(i).get(0) - margin * 3);
+            
+            Rect roi = new Rect(x, y, width, height);
+            
+            Mat cropped = img.clone();
+            
+            
+            
+            rois.add(cropped.submat(roi));
+        }
+
+        return rois;
+    }
+}
+
---
+++ Java/Opencv2Test/src/opencv2test/Opencv2Test.java
@@ -0,0 +1,96 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package opencv2test;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.InputStream;
+import java.util.ArrayList;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.WindowConstants;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfByte;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.imgproc.Imgproc;
+
+/**
+ *
+ * @author jasper
+ */
+public class Opencv2Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.load(new File("/opt/local/share/OpenCV/java/libopencv_java2410.dylib").getAbsolutePath());
+
+        Mat img = Highgui.imread("/Users/jasper/Desktop/img.jpg");
+
+	Mat grey = img.clone();
+
+	// Convert de afbeelding naar grijswaarden
+	Analyse.convertToGrey(grey);
+
+	// Isoleer de zwarte stukken
+	Analyse.thresholdISOBlack(grey, grey);
+
+	// Bereken voor alle Rows het gemiddelde aan pixels in de columns.
+	Mat rowCounted = Analyse.averageRows(grey);
+        Analyse.thresholdISOBlack(rowCounted, rowCounted);
+	
+	// Vind het aantal blobs en kijk of het 'systemen' zijn.
+	ArrayList<ArrayList<Integer>> blobList = Analyse.oneDimensionalHorizontalBlobFinder(rowCounted);
+	
+	//Uitsnedes maken van notenbalken en het kwardratische verticalegemiddelde berekenen per balk
+	ArrayList<Mat> rois = Analyse.getROIperBlob(blobList, img);
+    
+	for (Mat v : rois)
+	{
+            Mat filteredImage = Analyse.filterDifference(v, Analyse.averageRows(v));
+            
+            // Vind het aantal blobs en kijk of het 'noten' zijn.
+            ArrayList<ArrayList<Integer>> noteList = Analyse.oneDimensionalVerticalBlobFinder(Analyse.averageCols(filteredImage));
+
+            Analyse.drawOneDimensionalBlobsVertical(noteList, filteredImage);
+            
+            showResult(filteredImage);
+	}
+
+	//Teken gevonden blobs
+	Analyse.drawOneDimensionalBlobsHorizontal(blobList, img);
+
+        showResult(img);
+	//showResult(grey);
+        //showResult(rowCounted);
+    
+        //Highgui.imwrite("/Users/jasper/Desktop/imgx.jpg", rowCounted);
+    }
+
+    public static void showResult(Mat img) {
+        Imgproc.resize(img, img, img.size());
+        MatOfByte matOfByte = new MatOfByte();
+        Highgui.imencode(".jpg", img, matOfByte);
+        byte[] byteArray = matOfByte.toArray();
+        BufferedImage bufImage = null;
+        try {
+            InputStream in = new ByteArrayInputStream(byteArray);
+            bufImage = ImageIO.read(in);
+            JFrame frame = new JFrame();
+            frame.getContentPane().add(new JLabel(new ImageIcon(bufImage)));
+            frame.pack();
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
